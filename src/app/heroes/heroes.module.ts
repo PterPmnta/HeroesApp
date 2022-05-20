f62f751eece287fa7022d4e01e6896d9224c64e0
Pter Pmnta M <pedro.pimienta.morales@gmail.com>
2022-05-19 22:58:16 -0500

    section 12 final commit

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -5,6 +5,7 @@ import { BuscarComponent } from './pages/buscar/buscar.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ListadoComponent } from './pages/listado/listado.component';
 import { HeroeComponent } from './pages/heroe/heroe.component';
+import { HeroesRoutingModule } from './heroes-routing.module';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,6 @@ import { HeroeComponent } from './pages/heroe/heroe.component';
     ListadoComponent,
     HeroeComponent,
   ],
-  imports: [CommonModule],
+  imports: [CommonModule, HeroesRoutingModule],
 })
 export class HeroesModule {}
