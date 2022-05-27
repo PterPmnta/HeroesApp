28ad134739214551812763f2598f565029a38577
Pter Pmnta M <pedro.pimienta.morales@gmail.com>
2022-05-27 12:28:58 -0500

    add the user name to the navbar

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -2,16 +2,24 @@ import { environment } from './../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Auth } from '../interfaces/auth.interface';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   private baseUrl: string = environment.baseUrl;
+  private user: Auth | undefined;
+
+  get authUser(): Auth {
+    return { ...this.user! };
+  }
 
   constructor(private http: HttpClient) {}
 
   login() {
-    return this.http.get<Auth>(`${this.baseUrl}/usuario/1`);
+    return this.http
+      .get<Auth>(`${this.baseUrl}/usuarios/1`)
+      .pipe(tap((resp) => (this.user = resp)));
   }
 }
--- src/app/heroes/pages/home/home.component.ts
+++ src/app/heroes/pages/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../../../auth/services/auth.service';
+import { Auth } from '../../../auth/interfaces/auth.interface';
 
 @Component({
   selector: 'app-home',
@@ -9,11 +11,19 @@ import { Router } from '@angular/router';
       .heroes-container {
         margin: 10px;
       }
+
+      .margin-user {
+        margin-right: 10px;
+      }
     `,
   ],
 })
 export class HomeComponent implements OnInit {
-  constructor(private router: Router) {}
+  get authUser(): Auth {
+    return this.authService.authUser;
+  }
+
+  constructor(private router: Router, private authService: AuthService) {}
 
   ngOnInit(): void {}
 
