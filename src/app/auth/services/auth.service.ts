4f83ddc52379d6146f4b3048fa9404bf2a4c3074
Pter Pmnta M <pedro.pimienta.morales@gmail.com>
2022-05-27 16:29:47 -0500

    update guard and add new rxjs operators

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,8 +1,9 @@
 import { environment } from './../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map, tap } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
 import { Auth } from '../interfaces/auth.interface';
-import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -18,12 +19,27 @@ export class AuthService {
   constructor(private http: HttpClient) {}
 
   login() {
-    return this.http
-      .get<Auth>(`${this.baseUrl}/usuarios/1`)
-      .pipe(tap((resp) => (this.user = resp)));
+    return this.http.get<Auth>(`${this.baseUrl}/usuarios/1`).pipe(
+      tap((resp) => (this.user = resp)),
+      tap((resp) => localStorage.setItem('token', resp.id))
+    );
   }
 
   logout() {
+    localStorage.removeItem('token');
     this.user = undefined;
+  }
+
+  verificaAuth(): Observable<boolean> {
+    if (!localStorage.getItem('token')) {
+      return of(false);
+    }
+
+    return this.http.get<Auth>(`${this.baseUrl}/usuarios/1`).pipe(
+      map((auth) => {
+        this.user = auth;
+        return true;
+      })
+    );
   }
 }
